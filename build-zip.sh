950b9b86f9689d8f90322d7b1210dbee727e8ea3
vincemann <v.conrad98@web.de>
2020-12-15 11:39:56 +0100

    Close #4.

--- build-zip.sh
+++ build-zip.sh
@@ -6,27 +6,46 @@
 # before runnning this script you need to have built the fat jar with jdk8 enabled via
 # ./gradlew shadowJar
 # the jar is expected to be at /build/libs/Subtitle-Buddy-1.0.0-all.jar
-# javafx runtime is expected at /opt/javafx-sdk-11.0.2
-# download from https://gluonhq.com/products/javafx/ unzip and move to /opt/
-
+# javafx runtimes are expected at:
+# /opt/javafx-sdk-11.0.2;/opt/javafx-win-sdk-11.0.2;/opt/javafx-osx-sdk-11.0.2
+# download from https://gluonhq.com/products/javafx/ unzip, rename and move to /opt/
+# finshed zips will be at ./build/lib/subtitle-buddy-<os>.zip
 
 buildFolder="./build/libs"
-zipFolder="$buildFolder/subtitle-buddy"
-echo "removing old zip folder"
-rm -rf "$zipFolder"
-mkdir -p "$zipFolder"
+zipFolderWin="$buildFolder/subtitle-buddy-win"
+zipFolderLinux="$buildFolder/subtitle-buddy-linux"
+zipFolderOsx="$buildFolder/subtitle-buddy-osx"
 
-echo "copying fat jar into zip folder"
-cp "$buildFolder/Subtitle-Buddy-1.0.0-all.jar" "$zipFolder/Subtitle-Buddy-1.0.0-all.jar"
+echo "emptying old zip folders"
+rm -rf "$zipFolderWin"
+rm -rf "$zipFolderLinux"
+rm -rf "$zipFolderOsx"
+mkdir -p "$zipFolderWin"
+mkdir -p "$zipFolderLinux"
+mkdir -p "$zipFolderOsx"
 
-# copy javafx runtime into build folder
-echo "copying javafx sdk into zip folder"
-cp -R /opt/javafx-sdk-11.0.2/ "$zipFolder"
+echo "copying fat jar into zip folders"
+cp "$buildFolder/Subtitle-Buddy-1.0.0-all.jar" "$zipFolderWin/Subtitle-Buddy-1.0.0-all.jar"
+cp "$buildFolder/Subtitle-Buddy-1.0.0-all.jar" "$zipFolderLinux/Subtitle-Buddy-1.0.0-all.jar"
+cp "$buildFolder/Subtitle-Buddy-1.0.0-all.jar" "$zipFolderOsx/Subtitle-Buddy-1.0.0-all.jar"
+
+
+echo "copying javafx sdk into zip folders"
+cp -R /opt/javafx-win-sdk-11.0.2/ "$zipFolderWin/"
+cp -R /opt/javafx-sdk-11.0.2/ "$zipFolderLinux"
+cp -R /opt/javafx-osx-sdk-11.0.2/ "$zipFolderOsx"
+# rename skd folders to be uniform inside zip
+mv "$zipFolderWin/javafx-win-sdk-11.0.2" "$zipFolderWin/javafx-sdk-11.0.2"
+mv "$zipFolderOsx/javafx-osx-sdk-11.0.2" "$zipFolderOsx/javafx-sdk-11.0.2"
 
 echo "copying start-scripts into zip folder"
-cp start* "$zipFolder"
+cp start.sh "$zipFolderLinux"
+cp start.sh "$zipFolderOsx"
+cp start.bat "$zipFolderWin"
 
-echo "creating zip file..."
+echo "creating zip files"
 cd "$buildFolder"
-zip -r subtitle-buddy.zip subtitle-buddy
+zip -r subtitle-buddy-win.zip "subtitle-buddy-win"
+zip -r subtitle-buddy-linux.zip "subtitle-buddy-linux"
+zip -r subtitle-buddy-osx.zip "subtitle-buddy-osx"
 
