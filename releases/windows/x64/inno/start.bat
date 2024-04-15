c53f3b55e774ce6c4330cbbaed2e2042d08bd4e7
vincemann <v.conrad98@web.de>
2024-04-15 15:21:34 +0200

    create windows installer solution, regarding #6.

--- releases/windows/x64/inno/start.bat
+++ releases/windows/x64/inno/start.bat
@@ -1,5 +1,13 @@
 @echo off
 setlocal
+
+rem Set path to the Java executable
 set PATH=.\jre\bin;%PATH%
-java -jar application.jar
+
+rem Set the path to the JavaFX SDK
+set JAVAFX_SDK=.\lib\javafx-sdk\lib
+
+rem Run the application with the local JavaFX
+java --module-path %JAVAFX_SDK% --add-modules javafx.controls,javafx.fxml -jar application.jar
+
 endlocal
