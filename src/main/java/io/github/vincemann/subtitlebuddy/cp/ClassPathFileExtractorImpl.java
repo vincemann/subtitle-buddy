feb8beb9aff4275c3d96c07b68964113bf108a0a
vincemann <v.conrad98@web.de>
2024-04-26 18:34:36 +0200

    readd create runtime tasks

--- src/main/java/io/github/vincemann/subtitlebuddy/cp/ClassPathFileExtractorImpl.java
+++ src/main/java/io/github/vincemann/subtitlebuddy/cp/ClassPathFileExtractorImpl.java
@@ -1,6 +1,7 @@
 package io.github.vincemann.subtitlebuddy.cp;
 
 import com.google.inject.Singleton;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -18,10 +19,12 @@ import java.util.List;
  * Files inside the running jar are only accessible as a Stream, so they are copied to a temp file.
  */
 @Singleton
+@Slf4j
 public class ClassPathFileExtractorImpl implements ClassPathFileExtractor {
 
     public CopiedClassPathFile findOnClassPath(String relPath) throws IOException {
         URL resourceUrl = getClass().getClassLoader().getResource(relPath);
+        log.debug("classpath resource to load: " + resourceUrl);
         if (resourceUrl == null) {
             throw new IOException("Resource not found on classpath: " + relPath);
         }
