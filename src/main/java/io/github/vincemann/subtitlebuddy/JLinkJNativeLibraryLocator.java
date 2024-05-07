82327338ac3e5a01c9d94d6d7357b6bf9a028989
vincemann <v.conrad98@web.de>
2024-05-07 17:56:55 +0200

    impl jlink solution with gradle tasks

--- src/main/java/io/github/vincemann/subtitlebuddy/JLinkJNativeLibraryLocator.java
+++ src/main/java/io/github/vincemann/subtitlebuddy/JLinkJNativeLibraryLocator.java
@@ -1,14 +1,13 @@
 package io.github.vincemann.subtitlebuddy;
 
-import com.github.kwhat.jnativehook.GlobalScreen;
 import com.github.kwhat.jnativehook.NativeLibraryLocator;
 import com.github.kwhat.jnativehook.NativeSystem;
+import lombok.extern.log4j.Log4j2;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.logging.Logger;
 
 /**
  * Use this class for loading jnativehook native libs from within the image.
@@ -17,8 +16,8 @@ import java.util.logging.Logger;
  * Just copy them from the extracted jar into the image.
  * Must ensure that the appLauncher script of the jlink image always has its current working dir set to the root of the image!
  */
+@Log4j2
 public class JLinkJNativeLibraryLocator implements NativeLibraryLocator {
-    private static final Logger log = Logger.getLogger(GlobalScreen.class.getPackage().getName());
 
     /**
      * Perform default procedures to interface with the native library. These procedures include
@@ -29,28 +28,23 @@ public class JLinkJNativeLibraryLocator implements NativeLibraryLocator {
 
         String libName = System.getProperty("jnativehook.lib.name", "JNativeHook");
 
-        // Get the package name for the GlobalScreen.
-        String basePackage = GlobalScreen.class.getPackage().getName().replace('.', '/');
-
-        System.out.println("base package: " + basePackage);
-
         String libNativeArch = NativeSystem.getArchitecture().toString().toLowerCase();
 
-        System.out.println("arch: " + libNativeArch);
+        log.debug("arch: " + libNativeArch);
         String libNativeName = System
                 .mapLibraryName(libName) // Get what the system "thinks" the library name should be.
                 .replaceAll("\\.jnilib$", "\\.dylib"); // Hack for OS X JRE 1.6 and earlier.
-        System.out.println("libNativeName: " + libNativeName);
+        log.debug("libNativeName: " + libNativeName);
 
         String currentWorkingDir = System.getProperty("user.dir");
 
-        System.out.println("working dir: " + currentWorkingDir);
+        log.debug("working dir: " + currentWorkingDir);
         // Resource path for the native library.
         String libPath = currentWorkingDir + "/lib/jnativehook/" +
                 NativeSystem.getFamily().toString().toLowerCase() +
                 '/' + libNativeArch + '/' + libNativeName;
 
-        System.out.println("Resource path: " + libPath);
+        log.debug("Resource path: " + libPath);
 
         File libFile = new File(libPath);
 
@@ -58,7 +52,7 @@ public class JLinkJNativeLibraryLocator implements NativeLibraryLocator {
             throw new RuntimeException("Unable to locate JNI library at " + libFile.getPath() + "!\n");
         }
 
-        log.fine("Loading library: " + libFile.getPath() + ".\n");
+        log.debug("Loading library: " + libFile.getPath() + ".\n");
         libraries.add(libFile);
 
         return libraries.iterator();
