955886f3d690f0736d440250d625985e17020c6c
vincemann <v.conrad98@web.de>
2024-05-28 15:38:35 +0200

    minor changes

--- releases/windows/x64/inno/start.bat
+++ releases/windows/x64/inno/start.bat
@@ -10,8 +10,8 @@ set "DIR=%DIR:~0,-1%"
 :: Define the path to the Java executable
 set "JAVA_EXEC=%DIR%\image\bin\javaw"
 
-:: Change to the directory where the script resides
-cd /d "%DIR%"
+:: Change to the directory where the script resides, then into image root dir
+cd /d "%DIR%\image"
 
 start "" "%JAVA_EXEC%" %CDS_JVM_OPTS% -Djlink=true --add-reads org.apache.commons.configuration2=ALL-UNNAMED --add-reads io.github.vincemann.merged.module=org.apache.commons.logging --add-reads org.apache.commons.configuration2=io.github.vincemann.merged.module --add-exports org.apache.commons.logging/org.apache.commons.logging.impl=org.apache.commons.configuration2 -p "%~dp0/../app" -m io.github.vincemann.subtitlebuddy/io.github.vincemann.subtitlebuddy.Main  %* & popd
 
