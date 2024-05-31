9fcdd003e1a60c307cb87f63af9ad558dd70e1e7
vincemann <v.conrad98@web.de>
2024-05-31 15:28:39 +0200

    ci

--- ci/osx/ftp-upload-image.sh
+++ ci/osx/ftp-upload-image.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-# build homebrew mac tar ball and send via ftp
+# build homebrew mac tar ball and send via ftp to linux server for hosting
 
 ./gradlew clean jlinkZip
+# sometimes it fails and I need to execute again without clean
+./gradlew jlinkZip
 dir="build"
 file=$(ls "$dir"/*.zip 2> /dev/null | head -n 1)
 # Check if a file was found
 if [ -z "$file" ]; then
-    echo "No .tar.gz files found in $directory"
+    echo "No .zip files found in $directory"
     exit 1
 else
     # Extract the file name from the full path
