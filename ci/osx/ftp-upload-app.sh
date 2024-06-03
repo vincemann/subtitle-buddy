116d06bc163f39a50d582c6550145040f4fe022f
vincemann <v.conrad98@web.de>
2024-06-03 15:16:14 +0200

    add mac ci script

---
+++ ci/osx/ftp-upload-app.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# build mac .app and zips it up. Then send via ftp to linux server for hosting
+# needs to be executed on mac os
+
+./gradlew clean jpackage
+dir="build/jpackage"
+zip -r "$dir/subtitle-buddy-app.zip" "$dir/*.app" 
+file=$(ls "$dir"/*.zip 2> /dev/null | head -n 1)
+# Check if a file was found
+if [ -z "$file" ]; then
+    echo "No .zip files found in $directory"
+    exit 1
+else
+    # Extract the file name from the full path
+    filename=$(basename "$file")
+    echo "Found file: $filename"
+    ./ci/osx/ftp-upload.sh $dir $filename
+fi
