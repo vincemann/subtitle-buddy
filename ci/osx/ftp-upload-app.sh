b303560220f0f5344bd6187550933dfc5a9bb929
vincemann <v.conrad98@web.de>
2024-06-03 15:20:18 +0200

    fix mac ci script

--- ci/osx/ftp-upload-app.sh
+++ ci/osx/ftp-upload-app.sh
@@ -4,7 +4,7 @@
 
 ./gradlew clean jpackage
 dir="build/jpackage"
-zip -r "$dir/subtitle-buddy-app.zip" "$dir/*.app" 
+zip -r "$dir/subtitle-buddy-app.zip" "$dir/subtitle-buddy.app" 
 file=$(ls "$dir"/*.zip 2> /dev/null | head -n 1)
 # Check if a file was found
 if [ -z "$file" ]; then
