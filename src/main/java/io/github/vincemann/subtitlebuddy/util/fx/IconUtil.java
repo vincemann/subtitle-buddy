fc8e91cf888e0aa92763c235ccb846b3f85dab14
vincemann <v.conrad98@web.de>
2024-06-07 19:50:42 +0200

    make icons work on mac

--- src/main/java/io/github/vincemann/subtitlebuddy/util/fx/IconUtil.java
+++ src/main/java/io/github/vincemann/subtitlebuddy/util/fx/IconUtil.java
@@ -9,17 +9,31 @@ import java.io.InputStream;
 @Log4j2
 public class IconUtil {
 
-    public static void attachApplicationIconTo(Stage stage){
-        try {
-            // Load and set the application icon
-            InputStream inputStream = IconUtil.class.getClassLoader().getResourceAsStream("icon.png");
-            if (inputStream == null) {
-                throw new IllegalArgumentException("Icon not found");
+        public static void attachApplicationIconTo(Stage stage) {
+            try {
+                String iconPath;
+                if (isMacOS()) {
+                    iconPath = "icon.icns";
+                } else {
+                    iconPath = "icon.png";
+                }
+
+                // Load and set the application icon
+                InputStream inputStream = IconUtil.class.getClassLoader().getResourceAsStream(iconPath);
+                if (inputStream == null) {
+                    throw new IllegalArgumentException("Icon not found: " + iconPath);
+                }
+                Image icon = new Image(inputStream);
+                stage.getIcons().add(icon);
+            } catch (Exception e) {
+                log.error("could not load icon", e);
             }
-            Image icon = new Image(inputStream);
-            stage.getIcons().add(icon);
-        }catch (Exception e){
-            log.error("could not load icon",e);
+        }
+
+        private static boolean isMacOS() {
+            String osName = System.getProperty("os.name").toLowerCase();
+            return osName.contains("mac");
         }
     }
+
 }
