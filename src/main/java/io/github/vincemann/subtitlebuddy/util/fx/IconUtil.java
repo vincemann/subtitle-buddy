28570f7a1cb1477129e9c5204bbc38122f4eae75
vincemann <v.conrad98@web.de>
2024-06-07 19:51:21 +0200

    fix syntax error

--- src/main/java/io/github/vincemann/subtitlebuddy/util/fx/IconUtil.java
+++ src/main/java/io/github/vincemann/subtitlebuddy/util/fx/IconUtil.java
@@ -9,31 +9,31 @@ import java.io.InputStream;
 @Log4j2
 public class IconUtil {
 
-        public static void attachApplicationIconTo(Stage stage) {
-            try {
-                String iconPath;
-                if (isMacOS()) {
-                    iconPath = "icon.icns";
-                } else {
-                    iconPath = "icon.png";
-                }
-
-                // Load and set the application icon
-                InputStream inputStream = IconUtil.class.getClassLoader().getResourceAsStream(iconPath);
-                if (inputStream == null) {
-                    throw new IllegalArgumentException("Icon not found: " + iconPath);
-                }
-                Image icon = new Image(inputStream);
-                stage.getIcons().add(icon);
-            } catch (Exception e) {
-                log.error("could not load icon", e);
+    public static void attachApplicationIconTo(Stage stage) {
+        try {
+            String iconPath;
+            if (isMacOS()) {
+                iconPath = "icon.icns";
+            } else {
+                iconPath = "icon.png";
             }
-        }
 
-        private static boolean isMacOS() {
-            String osName = System.getProperty("os.name").toLowerCase();
-            return osName.contains("mac");
+            // Load and set the application icon
+            InputStream inputStream = IconUtil.class.getClassLoader().getResourceAsStream(iconPath);
+            if (inputStream == null) {
+                throw new IllegalArgumentException("Icon not found: " + iconPath);
+            }
+            Image icon = new Image(inputStream);
+            stage.getIcons().add(icon);
+        } catch (Exception e) {
+            log.error("could not load icon", e);
         }
     }
+
+    private static boolean isMacOS() {
+        String osName = System.getProperty("os.name").toLowerCase();
+        return osName.contains("mac");
+    }
+    
 
 }
