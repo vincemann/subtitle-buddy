5f9670936e65eac2ac2c0454923e51e3a6bcd067
vincemann <v.conrad98@web.de>
2024-06-12 14:55:05 +0200

    repeat sometimes failing task

--- ci/linux/test-manual-installation.sh
+++ ci/linux/test-manual-installation.sh
@@ -4,7 +4,17 @@
 # remove possible installation
 rm -f /usr/local/bin/subtitle-buddy
 
+
 ./gradlew jlinkZip
+
+# sometimes need to repeat for whatever reason
+if ls ./build/*.zip 1> /dev/null 2>&1; then
+    echo "Zip files found in the ./build directory."
+else
+    echo "No zip files found in the ./build directory. Running ./gradlew jlinkZip..."
+    ./gradlew jlinkZip
+fi
+
 
 cd build
 
